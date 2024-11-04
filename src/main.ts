8fec594adb9a85a5aeb3ad01a36edecdc19ce99e
Frank Shi <frshi@ucsc.edu>
2024-11-03 21:37:33 -0800

    L16 Dialog complete

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-// todo
+const button = document.createElement('button');
+button.textContent = 'Click me!';
+document.body.appendChild(button);
+
+button.addEventListener('click', () => {
+  alert('you clicked the button!');
+});
