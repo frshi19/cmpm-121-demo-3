56cfcd27802c2f6d71b00580e6847b7ac3cc59ce
Frank Shi <frshi@ucsc.edu>
2024-11-06 20:12:35 -0800

    fixed formatting issues

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-const button = document.createElement('button');
-button.textContent = 'Click me!';
+const button = document.createElement("button");
+button.textContent = "Click me!";
 document.body.appendChild(button);
 
-button.addEventListener('click', () => {
-  alert('you clicked the button!');
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
 });
